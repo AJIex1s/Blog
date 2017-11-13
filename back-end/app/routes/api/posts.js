8bed7bbe649e46af35ef8fa1a9570c35f297409c
AJIex1s <alexpeclug@mail.ru>
2017-11-13 09:50:34 +0300

    add route - modify post

--- back-end/app/routes/api/posts.js
+++ back-end/app/routes/api/posts.js
@@ -3,15 +3,20 @@ let mongoose = require('mongoose');
 let posts = require('../../static-data');
 var Post = mongoose.model('Post');
 
-router.param('id', (req, res, next, id) => {
-    if (!posts.find((post) => post.id == id))
+Post.findAliveById = function (id) {
+    return Post.findOne({ "id": id, deleted: false });
+}
+
+router.param('id', async (req, res, next, id) => {
+    let post = await Post.findAliveById(id);
+    if (!post)
         return res.sendStatus(404);
     return next();
 });
 
 router.route('/')
     .get((req, res) => {
-        Post.find().then(posts => res.json(posts));
+        return Post.find().then(posts => res.json(posts));
     })
     .post(async (req, res) => {
         if (!req.body || !req.body.post)
@@ -27,20 +32,33 @@ router.route('/')
         post = new Post(req.body.post);
         post.id = postId;
 
-        return post.save().then(function () {
-            console.log(post.updateDate);
-            return res.json(post);
-        });
-
+        return post.save().then(() => res.json(post));
     });
 
 router.route('/:id')
-    .get((req, res, next) => {
-        let post = posts.find(post => post.id == req.params['id']);
+    .get(async (req, res, next) => {
+        let post = await Post.findAliveById(req.params.id);
         return res.json(post);
     })
-    .put((req, res, next) => {
-        let post = posts.find(post => post.id == req.params['id']);
+    .put(async (req, res, next) => {
+        if (!req.body || !req.body.post)
+            return res.sendStatus(400);
+
+        let post = await Post.findAliveById(req.params.id);
+
+        console.log(req.body.post.title);
+        if (typeof req.body.post.title !== 'undefined')
+            post.title = req.body.post.title;
+        if (typeof req.body.post.content !== 'undefined')
+            post.content = req.body.post.content;
+
+
+        return post.save().then(() => res.json(post));
+    })
+    .delete(async (req, res) => {
+        let post = await Post.findOne({ "id": req.params['id'], delete: false });
+        post.deleted = true;
+        post.deleteDate = Date.now();
         return res.json(post);
     });
 
