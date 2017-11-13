1a4f81e6dea413d1b1157024e06465f16359704e
AJIex1s <alexpeclug@mail.ru>
2017-11-13 16:23:38 +0300

    add simple front-end main page

---
+++ front-end/src/app/posts-list/post-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'post-list',
+    templateUrl: 'post-list.component.html',
+    styleUrls: ['post-list.component.css']
+})
+class PostListComponent { }
